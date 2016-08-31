6312bdee3c814db46ad8e465dc926a6ee5777d87
Mathias Brandt <maini123@gmail.com>
2016-08-31 23:41:25 +0200

    profile picture and some info is shown in profile activity

--- FirstImpressions/app/src/main/java/com/mathiasbrandt/firstimpressions/LoginActivity.java
+++ FirstImpressions/app/src/main/java/com/mathiasbrandt/firstimpressions/LoginActivity.java
@@ -3,13 +3,22 @@ package com.mathiasbrandt.firstimpressions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import static com.firebase.ui.auth.ui.AcquireEmailHelper.RC_SIGN_IN;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         if(firebaseAuth.getCurrentUser() != null) {
             // session exists
+            saveFacebookId(); // TODO: delete this
+            goToProfileActivity();
         } else {
             startActivityForResult(
                     AuthUI.getInstance()
@@ -38,11 +49,30 @@ public class LoginActivity extends AppCompatActivity {
         if(requestCode == RC_SIGN_IN) {
             if(resultCode == RESULT_OK) {
                 // user signed in
-                startActivity(new Intent(this, ProfileActivity.class));
-                finish();
+                saveFacebookId();
+                goToProfileActivity();
             } else {
                 // user sign in failed
             }
         }
+    }
+
+    private void saveFacebookId() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference usersDatabase = FirebaseDatabase.getInstance().getReference("users");
+        String facebookId = "";
+
+        for(UserInfo profile : user.getProviderData()) {
+            if(profile.getProviderId().equals(getString(R.string.facebook_provider_id))) {
+                facebookId = profile.getUid();
+            }
+        }
+
+        usersDatabase.child(user.getUid()).child(getString(R.string.facebook_user_id)).setValue(facebookId);
+    }
+
+    private void goToProfileActivity() {
+        startActivity(new Intent(this, ProfileActivity.class));
+        finish();
     }
 }
--- FirstImpressions/app/src/main/java/com/mathiasbrandt/firstimpressions/ProfileActivity.java
+++ FirstImpressions/app/src/main/java/com/mathiasbrandt/firstimpressions/ProfileActivity.java
@@ -1,13 +1,103 @@
 package com.mathiasbrandt.firstimpressions;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
+    public static final String TAG = "ProfileActivity";
+
+    private DatabaseReference usersDatabase;
+    private FirebaseUser currentUser;
+
+    private ImageView profilePicture;
+    private TextView fullName;
+    private TextView age;
+    private TextView location;
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        usersDatabase = FirebaseDatabase.getInstance().getReference(getString(R.string.users_database_reference));
+
+        profilePicture = (ImageView) findViewById(R.id.image_profile_picture);
+        fullName = (TextView) findViewById(R.id.textViewFullName);
+        age = (TextView) findViewById(R.id.textViewAge);
+        location = (TextView) findViewById(R.id.textViewLocation);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        setProfileImage();
+
+        
+
+        fullName.setText(currentUser.getDisplayName());
+        age.setText("");
+        location.setText("");
+
+        for(UserInfo info : currentUser.getProviderData()) {
+            Log.d(TAG, info.toString());
+        }
+    }
+
+    private void setProfileImage() {
+        usersDatabase.child(currentUser.getUid()).child(getString(R.string.facebook_user_id)).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String facebookId = dataSnapshot.getValue(String.class);
+
+                String photoUrl = "https://graph.facebook.com/" + facebookId + "/picture?height=500";
+
+                Picasso.with(ProfileActivity.this).load(photoUrl).into(profilePicture);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "Could not get Facebook user id from database");
+            }
+        });
+    }
+
+    public void buttonLogoutClick(View v) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                        finish();
+                    }
+                });
     }
 }
